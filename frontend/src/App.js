257f13e2a06e617fb6f8803e8c96afd4c816f334
AghaDev <mohamadagha2@gmail.com>
2023-12-01 16:28:28 +0200

    Finished the Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 import Profile from '../src/Components/Profile/Profile.js'
+import Login from '../src/Pages/Login/Login.js'
+import RegistrationOne from "./Pages/Registration-one/Registration-one.js";
+import RegistrationTwo from "./Pages/Registration-two/Registration-two.js";
+import Home from "./Pages/Home/Home.js";
 function App() {
   return (
     <Router>
       <div className="App">
-  <Profile/>
+      <Home />
+      <Login />
+      <RegistrationOne />
+      <RegistrationTwo />
     </div>
     </Router>
     
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Login from './Pages/Login/Login';
+import App from './App.js';
+import Login from './Pages/Login/Login.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
