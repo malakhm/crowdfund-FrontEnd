f15a6cefbc72391944d203e6f60c71a97df0b257
malakhm <malakhmm22@gmail.com>
2023-12-02 17:16:11 +0200

    campaign card done!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 import Profile from '../src/Components/Profile/Profile.js'
-
+import Cards from '../src/Components/Campaign-card/Campaign-card.js'
 import Login from '../src/Pages/Login/Login.js'
 import RegistrationOne from "./Pages/Registration-one/Registration-one.js";
 import RegistrationTwo from "./Pages/Registration-two/Registration-two.js";
@@ -12,19 +12,20 @@ import Buttonb from "./Components/Button-black/Button.js";
 import SideBar from '../src/Components/Side-bar/Side-bar.js'
 import DonorMenu from "./Components/Donor-menu/donor-menu";
 import Header from './Components/Header/Header.js'
-
+import Feed from '../src/Pages/Feed/Feed.js'
 function App() {
   return (
     <Router>
       <div className="App">
 
-      <Home />
-      <Login />
+      {/* <Home /> */}
+   
       {/* <Buttonb>Yes, delete</Buttonb> */}
-      <RegistrationOne />
-      <RegistrationTwo />
+      {/* <RegistrationOne />
+      <RegistrationTwo /> */}
         <SideBar><DonorMenu/></SideBar>
-          <Profile/>
+        <Feed/>
+        
     </div>
     </Router>
     
---
+++ frontend/src/Pages/Feed/Feed.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Cards from '../../Components/Campaign-card/Campaign-card'
+const Feed = () => {
+  return (
+    <div className='Feed-main-Container'>
+      
+    </div>
+  )
+}
+
+export default Feed
