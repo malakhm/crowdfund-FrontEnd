877ab072e72e779a7f67c7ef0a8d0eb5ac5258fe
AghaDev <mohamadagha2@gmail.com>
2023-12-04 15:33:11 +0200

    Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom';
 import Profile from '../src/Components/Profile/Profile.js'
+import AddCampaign from '../src/Pages/Creator/Add-campaign/Add-campaign.js';
 import Login from '../src/Pages/Login/Login.js'
 import RegistrationOne from "./Pages/Registration-one/Registration-one.js";
 import RegistrationTwo from "./Pages/Registration-two/Registration-two.js";
 import Home from "./Pages/Home/Home.js";
 import Buttonb from "./Components/Button-black/Button.js";
-import AddCampaign from "./Pages/Creator/Add-campaign/Add-campaign.js";
-import SideBar from "../src/Components/Side-bar/Side-bar.js";
+
+import SideBar from '../src/Components/Side-bar/Side-bar.js'
+import DonorMenu from "./Components/Donor-menu/donor-menu";
+import Header from './Components/Header/Header.js'
+
 function App() {
   return (
     <Router>
       <div className="App">
-   
+      {/* <Home /> */}
+      {/* <Login /> */}
       {/* <Buttonb>Yes, delete</Buttonb> */}
+      {/* <RegistrationOne /> */}
+      {/* <RegistrationTwo /> */}
       <SideBar />
       <AddCampaign />
     </div>
