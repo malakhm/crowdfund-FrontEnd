392eb3e8a33f0bdbc1ecbab0b9c0bd3923c1ab25
malakhm <malakhmm22@gmail.com>
2023-12-08 15:11:38 +0200

    Revert "Merge pull request #10 from malakhm/new-moe-agha"
    This reverts commit 680f4d853973118ef50a11ec935f483a13d22eb0, reversing
    changes made to 29875c1c9e7d04adbe2b50b4ce61374e77773271.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Login from './Pages/Login/Login';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
