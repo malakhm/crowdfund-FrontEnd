acff30950d71e774856b1b713dc1fd9a68e1bd4b
Kamal <kamal_dia@hotmail.com>
2023-12-11 01:30:09 +0200

    pulling

--- frontend/src/Pages/Notifications/Notifications.js
+++ frontend/src/Pages/Notifications/Notifications.js
@@ -1,23 +1,40 @@
-import React from 'react'
-import Notification from '../../Components/Notification/Notification'
-import './Notifications.css'
-function Notifications() {
-  return (
-    <div className='Notifications-main-container-div container d-flex justify-self-center flex-column'>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-    </div>
-  )
-}
+// import io from "socket.io-client";
+// import "./Notifications.css";
 
-export default Notifications
+// const Notifications = () => {
+//   const socket = io("http://localhost:3001");
+
+//   const form = document.getElementById("form");
+//   const input = document.getElementById("input");
+//   const messages = document.getElementById("messages");
+
+//   form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     if (input.value) {
+//       socket.emit("send_notification", input.value);
+//       input.value = "";
+//     }
+//   });
+
+//   socket.on("send_notification", (msg) => {
+//     const item = document.createElement("li");
+//     item.textContent = msg;
+//     messages.appendChild(item);
+//     window.scrollTo(0, document.body.scrollHeight);
+//   });
+
+//   socket.on("receive_notification", (msg) => {
+//     console.log("this is it :", msg);
+//   });
+//   return (
+//     <div className="Notifications-big-container">
+//       <ul id="messages"></ul>
+//       <form id="form" action="">
+//         <input id="input" autocomplete="off" />
+//         <button>Send</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default Notifications;
