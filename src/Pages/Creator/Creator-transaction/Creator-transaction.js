1f542595895f2bd7ae6c23c4f7ee4df363cb7bc4
malakhm <malakhmm22@gmail.com>
2023-12-10 18:49:06 +0200

    notifications fetching done

--- src/Pages/Creator/Creator-transaction/Creator-transaction.js
+++ src/Pages/Creator/Creator-transaction/Creator-transaction.js
@@ -1,37 +1,53 @@
 import React from 'react'
-import Notification from '../../../Components/Notification/Notification'
+import Notification from '../../../Components/Notification/Notification.js'
 import { IoCheckmarkDoneCircle } from "react-icons/io5";
+import { useState, useEffect , useContext} from 'react';
+import { AuthContext } from '../../../Context/AuthContext';
+import axios from 'axios';
+import {toast} from 'react-toastify'
 import '../../Creator/Creator-transaction/Creator-transaction.css'
 const CreatorTransaction = () => {
+
+  const {user, token} = useContext(AuthContext)
+  const [data, setData] = useState([])
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    // Other headers if needed
+  };
+  const fetchData = async()=>{
+    try{
+    const response = await axios.get(`http://localhost:8100/api/campaignRoute/getById/${user.id}`, { headers })
+    if(response.status === 200){
+      setData(response.data.data)
+    }}
+    catch(error){
+      toast.error('something went wrong ERROR')
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
+console.log(data)
   return (
     <div className='Transaction-main-container container d-flex flex-column'>
-        <div className='transaction-stats-main'>
-
+         <div className='notifications-wrapper-div '>
+        
+        {data.map((item)=>(
+          console.log(item),
+        <Notification
+        key={item.id}
+        amount={item.amount}
+        campaignName={item.campaign_name}
+        >
+      <p>{item.user.username}</p>
+        </Notification>
+))}
         </div>
-        <Notification>
-      <p><IoCheckmarkDoneCircle className='TRANSACTION-IS-DONE-ICON'/> Malak</p>
-        </Notification>
-
-        <Notification>
-      <p><IoCheckmarkDoneCircle className='TRANSACTION-IS-DONE-ICON'/> Malak</p>
-        </Notification>
-
-        <Notification>
-      <p><IoCheckmarkDoneCircle className='TRANSACTION-IS-DONE-ICON'/> Malak</p>
-        </Notification>
-
-        <Notification>
-      <p><IoCheckmarkDoneCircle className='TRANSACTION-IS-DONE-ICON'/> Malak</p>
-        </Notification>
-        <Notification>
-      <p><IoCheckmarkDoneCircle className='TRANSACTION-IS-DONE-ICON'/> Malak</p>
-        </Notification>
-        <Notification>
-      <p><IoCheckmarkDoneCircle className='TRANSACTION-IS-DONE-ICON'/> Malak</p>
-        </Notification>
-    
     </div>
   )
 }
 
 export default CreatorTransaction
+
--- src/Pages/Notifications/Notifications.js
+++ src/Pages/Notifications/Notifications.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useState, useEffect } from 'react';
+import { AuthContext } from '../../Context/AuthContext.js';
+import axios from 'axios';
+import {toast} from 'react-toastify'
 import Notification from '../../Components/Notification/Notification'
 import '../Creator/Creator-transaction/Creator-transaction.css'
 function Notifications() {
+  const {user, token} = useContext(AuthContext)
+  const [data, setData] = useState([])
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    // Other headers if needed
+  };
+  const fetchData = async()=>{
+    try{
+    const response = await axios.get(`http://localhost:8100/api/donationRoute`, { headers })
+    if(response.status === 200){
+      setData(response.data.data)
+    }}
+    catch(error){
+      toast.error('something went wrong ERROR')
+    }
+
+  }
+
+  useEffect(() => {
+ 
+    fetchData()
+  }, []);
+console.log(data)
+
   return (
+
     <div className='Transaction-main-container container d-flex justify-self-center flex-column'>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
-      <Notification>Kamal</Notification>
+        <div className='notifications-wrapper-div '>
+
+          {data.map((item)=>(
+          <Notification key={item.id} amount={item.amount}
+          campaignName={item.Campaign.campaign_name}>{item.user.username}</Notification>
+
+          ))}
+ </div>
+      
+     
+      
    
    
     </div>
